9c407e6c03d3c25d13af8d6129cadd84a6e92d19
mnelson <mark.nelson3@mail.mcgill.ca>
2020-05-25 16:21:03 -0700

    mod DataRead.py & + M01_ModelForm
    Completed DataRead.py & saved nested dict as .json & .txt files.
      note: file size very large. Need to chop EEG data not needed for model.
    M01_ModelForm initialized. Will begin tomorrow.

--- scripts/Predict_Recog/DataRead.py
+++ scripts/Predict_Recog/DataRead.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-  This script is a first attempt at developing a machine learning algorithm to
-predict subsequent novel recognition from single trial EEG amplitude recorded
-at stimulus encoding. This data comes from the ROGER dataset related to my
-master thesis (2019). The steps are as follows:
+  This script reads in a nested array in .mat format and converts it to a nested dictionary.
     
-    (1) Read in EEG data 
+    (1) Read in EEG data (.mat file)
         - ~/Desktop/thesis/Data/ALLEEG_ODDSONLY.mat = 5 midline electrodes (AFz-Pz)
           for oddball trials only
           
-    (2) 
+    (2) Convert to nested dictionary: subj_dict[<VPn>]["Novels"][<nvl_n>]["Elecs"][<'Elec_name'>]["data"]
+        * Don't forget extra '' around electrode name
+        * need to convert numpy file types to be json compatible
+        
+    (3) Save nested dictionary to json file in /MarkNelson86_EEGRecogBIDS/data/
 
 Created on Wed May 20 14:03:00 2020
 
@@ -18,28 +19,27 @@ Created on Wed May 20 14:03:00 2020
 """
 
 import numpy as np
-import pandas as pd
 from scipy.io import loadmat
 
-## Useful functions for .mat to pd.DataFrame
-def coerce_void(value):
-    """
-    Converts `value` to `value.dtype`
-
-    Parameters
-    ----------
-    value : array_like
-
-    Returns
-    -------
-    value : dtype
-        `Value` coerced to `dtype`
-    """
-
-    if np.squeeze(value).ndim == 0:
-        return value.dtype.type(value.squeeze())
-    else:
-        return np.squeeze(value)
+## Useful functions for .mat conversion
+#def coerce_void(value):
+#    """
+#    Converts `value` to `value.dtype`
+#
+#    Parameters
+#    ----------
+#    value : array_like
+#
+#    Returns
+#    -------
+#    value : dtype
+#        `Value` coerced to `dtype`
+#    """
+#
+#    if np.squeeze(value).ndim == 0:
+#        return value.dtype.type(value.squeeze())
+#    else:
+#        return np.squeeze(value)
 
 
 def get_labels(fields):
@@ -88,6 +88,7 @@ fn_Trl = FieldNames_Nvls.index('Trln')
 fn_TsT = FieldNames_Nvls.index('Trls_since_tar')
 fn_TsO = FieldNames_Nvls.index('Trls_since_odd')
 
+# loop through data structure levels and store values in nested dictionary
 subj_dict = {}
 for VP in range(len(ID_Str)):                                                   # iterate through subjs
     
@@ -99,33 +100,68 @@ for VP in range(len(ID_Str)):                                                   
         
         for e in range(len(data[0][VP][fn_N][0][Nn][fn_E][0])):                 # iterate through electrodes
             
-            Delec = data[0][VP][fn_N][0][Nn][fn_E][0][e][fn_D][0].astype(float)
-            Enum = Elec_Numbs[e]
+            Delec = data[0][VP][fn_N][0][Nn][fn_E][0][e][fn_D][0].tolist()      # convert to type: list
+            Enum = Elec_Numbs[e].tolist()
             Ename = Elec_Names[e]
-            elec_dict[Ename] = {"number" : Enum, "data" : Delec}                # Build Elec dict
+            elec_dict[Ename] = {"name" : Ename, "number" : Enum, "data" : Delec} # Build Elec dict
         
         # Build dictionary for novels each with enclosed electrode dictionary
-        Trln = data[0][VP][fn_N][0][Nn][fn_Trl][0][0]
-        TrlSncTar = data[0][VP][fn_N][0][Nn][fn_TsT][0][0]
-        TrlSncOdd = data[0][VP][fn_N][0][Nn][fn_TsO][0][0]
-        novel_dict['NVL_'+str(Nn)] = {"Trln": Trln, "Trls_since_tar": TrlSncTar, "Trls_since_odd": TrlSncOdd, "Elecs" : elec_dict}
-        
+        Trln = data[0][VP][fn_N][0][Nn][fn_Trl][0][0].tolist()                  # convert to type: int
+        TrlSncTar = data[0][VP][fn_N][0][Nn][fn_TsT][0][0].tolist()
+        TrlSncOdd = data[0][VP][fn_N][0][Nn][fn_TsO][0][0].tolist()
+        novel_dict['nvl_'+str(Nn+1)] = {"Trln": Trln, "Trls_since_tar": TrlSncTar, "Trls_since_odd": TrlSncOdd, "Elecs" : elec_dict}
+
     # Build dictionary for subjects each with enclosed novels & electrodes dictionaries
-    Subj_ID = 
+    Subj_ID = data[0][VP][0][0]
+    subj_dict[Subj_ID] = {"Novels" : novel_dict}
+    
+    
+
+## Save dictionary object 
+import os.path
+import json
+save_path = '/Users/mheado86/Desktop/course-materials-2020/project/MarkNelson86_EEGRecogBIDS/data/'
+file_name = 'subj_dict'
+
+# save as .json (numpy data types not compatible with json)
+complete_name = os.path.join(save_path, file_name+'.json')
+json = json.dumps(subj_dict)
+f = open(complete_name,"w")
+f.write(json)
+f.close()
+
+# save as .txt
+complete_name = os.path.join(save_path, file_name+'.txt')
+f = open(complete_name,"w")
+f.write(str(subj_dict))
+f.close()
+
+## GUIDE to nested dictionary object: subj_dict["VPn"]["Novels"]["nvl_n"]["Elecs"]["'Elec_name'"]["data"]
+# keys_subj = dict.keys(subj_dict)
+
+## Pandas DataFrame from nested dictionary: THIS DOESN'T MAKE SENSE FOR MY DATA??
+#df_test = pd.DataFrame.from_dict({(i,j,k): subj_dict[i]["Novels"][j]["Elecs"][k] 
+#                               for i in subj_dict.keys() 
+#                               for j in subj_dict[i]["Novels"].keys()},
+#                               orient='index')
 
 
+
+
+
+
+
+## SCRATCH: HOW TO WORK WITH NESTED MATLAB STRUCTURES IN PYTHON???
+    
 # Convert data structure to a normal dictionary using dtypes as keys (NEED TO BUILD OUT FROM CORE)
-data_dict = {FieldNames_Subj[n]: value for n, value in enumerate(data)}
+# data_dict = {FieldNames_Subj[n]: value for n, value in enumerate(data)}
+    
 # data2['ID'][subn][Sfn][0][NVLn][NVLfn][0][En][Efn][0][Edata]
 # data2['ID'][900][1][0][40][6][0][4][2][0][500]
 # Can't access other keys (Novels, BFail)
 
 # To coerce array-like values to be arrays: helpful?
-data3['ID'] = coerce_void(data_dict['ID'])
-
-
-
-## SCRATCH: HOW TO WORK WITH NESTED MATLAB STRUCTURES IN PYTHON???
+# data3['ID'] = coerce_void(data_dict['ID'])
 
 # Accessing values directly in numpy void object: long form                      
 # data[0][subn][Sfn][0][NVLn][NVLfn][0][En][Efn][0][Edata]
@@ -162,8 +198,8 @@ data3['ID'] = coerce_void(data_dict['ID'])
 #ID_Str = [np.array2string(x) for x in S['S'][0][:]['ID']]         # S(:).ID extracted
 #Nvls_npy = [NVLS for NVLS in S['S'][0][:]['Novels']]              # S(:).Novels extracted
 
-column_names = ['Type', 'Trln', 'Time_since_tar', 'Trls_since_tar', 'Time_since_odd', 'Trls_since_odd']
-X = pd.DataFrame(Nvls_npy, columns=column_names) 
+#column_names = ['Type', 'Trln', 'Time_since_tar', 'Trls_since_tar', 'Time_since_odd', 'Trls_since_odd']
+#X = pd.DataFrame(Nvls_npy, columns=column_names) 
 
 
 # Extracting Trln values for all Novels for VP0001 (Could turn this into a nested list for all subs!)
@@ -175,34 +211,34 @@ X = pd.DataFrame(Nvls_npy, columns=column_names)
 #    Trln_VP0001_values.append(Trln_nmpy_VP0001[0][i][0][0])
 
 # nested list extracting trln for all subs
-Trln_Vals_All = []
-for SUB in range(0, len(Nvls_npy)):
-    Trln_nmpy_SUBn = Nvls_npy[SUB]['Trln']
-    TempList = []
-    
-    for NVL in range(0, len(Trln_nmpy_SUBn[0])):
-        TempList.append(Trln_nmpy_SUBn[0][NVL][0][0])
-        
-    Trln_Vals_All.append(TempList)
+#Trln_Vals_All = []
+#for SUB in range(0, len(Nvls_npy)):
+#    Trln_nmpy_SUBn = Nvls_npy[SUB]['Trln']
+#    TempList = []
+#    
+#    for NVL in range(0, len(Trln_nmpy_SUBn[0])):
+#        TempList.append(Trln_nmpy_SUBn[0][NVL][0][0])
+#        
+#    Trln_Vals_All.append(TempList)
     
 # nested list extracting Trls_since_tar for all subs
-TrlsSncTar_Vals_All = []
-for SUB in range(0, len(Nvls_npy)):
-    TrlsSncTar_nmpy_SUBn = Nvls_npy[SUB]['Trls_since_tar']
-    TempList = []
+#TrlsSncTar_Vals_All = []
+#for SUB in range(0, len(Nvls_npy)):
+#    TrlsSncTar_nmpy_SUBn = Nvls_npy[SUB]['Trls_since_tar']
+#    TempList = []
+#    
+#    for NVL in range(0, len(TrlsSncTar_nmpy_SUBn[0])):
+#        TempList.append(TrlsSncTar_nmpy_SUBn[0][NVL][0][0])
+#        
+#    TrlsSncTar_Vals_All.append(TempList)
     
-    for NVL in range(0, len(TrlsSncTar_nmpy_SUBn[0])):
-        TempList.append(TrlsSncTar_nmpy_SUBn[0][NVL][0][0])
-        
-    TrlsSncTar_Vals_All.append(TempList)
-    
-# Extracting electrodes
-Elecs_AllNvl_VP0001 = Nvls_npy[0]['Elecs']
-Data_Nvl1_VP0001 = Elecs_AllNvl_VP0001[0][0]['data']
-Data_Elec1_Nvl1_VP0001 = Data_Nvl1_VP0001[0][0]
-Data_Elec2_Nvl1_VP0001 = Data_Nvl1_VP0001[0][1]
-
-
-## Convert using DataRead_CAT.py
-from DataRead_CAT import CatsReader as cr
+## Extracting electrodes
+#Elecs_AllNvl_VP0001 = Nvls_npy[0]['Elecs']
+#Data_Nvl1_VP0001 = Elecs_AllNvl_VP0001[0][0]['data']
+#Data_Elec1_Nvl1_VP0001 = Data_Nvl1_VP0001[0][0]
+#Data_Elec2_Nvl1_VP0001 = Data_Nvl1_VP0001[0][1]
+#
+#
+### Convert using DataRead_CAT.py
+#from DataRead_CAT import CatsReader as cr
 
---
+++ scripts/Predict_Recog/M01_ModelForm.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+  This script builds a SVR (Support Vector Regression) machine learning model from 
+EEG data related to Oddball_Recog_2019 in order to predict object recognition
+in phase 2 (recognition test) of the task from single trial EEG amplitude recorded 
+in phase 1 (oddball, stimulus encoding). 
+
+    Features:
+        Single trial mean EEG amplitude 
+        
+    Labels:
+        Behavioral outcome (Recognized: 1, not-recognized: 0)
+
+Created on Mon May 25 16:08:53 2020
+
+@author: mheado86
+"""
+
