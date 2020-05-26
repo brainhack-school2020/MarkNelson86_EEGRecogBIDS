b9bc103ca5a39d10a2693f99b2196d95e3edade9
mnelson <mark.nelson3@mail.mcgill.ca>
2020-05-26 09:37:35 -0700

    ~ M01_ModelForm
    (1) data load (finish)
    (2) feature extraction (start)

--- scripts/Predict_Recog/M01_ModelForm.py
+++ scripts/Predict_Recog/M01_ModelForm.py
@@ -11,9 +11,31 @@ in phase 1 (oddball, stimulus encoding).
         
     Labels:
         Behavioral outcome (Recognized: 1, not-recognized: 0)
+        
+The following steps are performed:
+    (1) load data dictionary: data['VP0001']['Novels']['nvl_1']['Elecs']["'Cz'"]['data']
+    (2) Extract features
 
 Created on Mon May 25 16:08:53 2020
 
 @author: mheado86
 """
+
+import os.path 
+import json                                                                     # json.load()
+import nilearn
+
+## Load data ##
+
+filepath = '/Users/mheado86/Desktop/course-materials-2020/project/MarkNelson86_EEGRecogBIDS/data/'
+file_name = 'subj_dict'
+full_fn_json = os.path.join(filepath, file_name+'.json')
+
+with open(full_fn_json) as file:
+  data = json.load(file)
+  
+  
+## Extract features ##
+  
+# Feature 1: Single trial EEG amplitude at Cz
 
